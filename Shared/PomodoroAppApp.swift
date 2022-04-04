d14d6e638cecd1c842c9bb616da584e9bb623566
Herie <heriemarie@gmail.com>
2022-04-04 21:47:45 +0800

    Initial Commit

---
+++ Shared/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  Shared
+//
+//  Created by Herie Marie A. Estaño on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
